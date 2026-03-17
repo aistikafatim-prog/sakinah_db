b95e3f4f6f04320c22cf81b4314053f5b8ce49c2
aistikafatim-prog <aistikafatim@gmail.com>
2026-03-17 13:20:59 +0700

    Update database.js
    Updated database configuration to include SSL options and new environment variable names.

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,28 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const db = new Sequelize(process.env.DB_NAME || 'sakinah_db', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
+//     host: process.env.DB_HOST || 'localhost',
+//     dialect: 'mysql',
+//     logging: false,
+// });
+
+// module.exports = db;
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
-const db = new Sequelize(process.env.DB_NAME || 'sakinah_db', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
-    host: process.env.DB_HOST || 'localhost',
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    logging: false,
-});
-
-module.exports = db;
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
