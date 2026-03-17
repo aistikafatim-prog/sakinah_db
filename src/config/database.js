eb0e59b97cf8242b83947f563379593084ce26da
aistikafatim-prog <aistikafatim@gmail.com>
2026-03-17 13:29:13 +0700

    Refactor database configuration for Sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,6 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
 
-// const db = new Sequelize(process.env.DB_NAME || 'sakinah_db', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
-//     host: process.env.DB_HOST || 'localhost',
-//     dialect: 'mysql',
-//     logging: false,
-// });
-
-// module.exports = db;
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+//require('dotenv').config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -26,3 +17,13 @@ const db = new Sequelize(
     }
   }
 );
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const db = new Sequelize(process.env.DB_NAME || 'sakinah_db', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
+//     host: process.env.DB_HOST || 'localhost',
+//     dialect: 'mysql',
+//     logging: false,
+// });
+
+// module.exports = db;
