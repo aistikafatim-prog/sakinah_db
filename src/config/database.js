ba78f51e55c45e194df01a03c680cd614e3e5d73
aistikafatim-prog <aistikafatim@gmail.com>
2026-03-17 13:38:47 +0700

    Refactor database.js to use dotenv and clean up code

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 
 const { Sequelize } = require('sequelize');
-//require('dotenv').config();
+require('dotenv').config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -17,6 +17,7 @@ const db = new Sequelize(
     }
   }
 );
+module.exports = db;
 // const { Sequelize } = require('sequelize');
 // require('dotenv').config();
 
