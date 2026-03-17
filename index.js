d3032d64e1555b06099b7a75c8813509c4cacc96
aistikafatim-prog <aistikafatim@gmail.com>
2026-03-17 16:05:02 +0700

    Refactor index.js for improved structure and logging

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const templateRoutes = require('./src/routes/templateRoutes');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'https://sakinahid.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
